1d75cb29f02818216001ee0fb69fdbecbe9e4274
Abhishek Kumar <abhishek1168953@gmail.com>
2026-05-07 03:57:12 +0530

    feat(auth): add social login

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,23 @@
 export default {
-  extends: ["@commitlint/config-conventional"],
+  extends: ['@commitlint/config-conventional'],
+
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'refactor',
+        'style',
+        'docs',
+        'test',
+        'chore',
+      ],
+    ],
+
+    'subject-case': [2, 'never', ['upper-case']],
+
+    'subject-empty': [2, 'never'],
+  },
 };
